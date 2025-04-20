d45aa93e6def656f5e09ad5daeea80de669878bc
mohammedhammad1 <mhijjawy3@gmail.com>
2025-04-21 02:26:17 +0300

    update

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main () {
+  
+  String myname = "mohammed";
+ int myage = 34;
+ double myhighest = -179.5;
+
+  print(myname[0].toUpperCase());
+  print (myage);
+  print (myhighest.ceil());
+  print (myhighest.floor());
+  print (myhighest.round());
+  print (myhighest.isNegative);
+  
+  
+  print(myhighest.abs());
+
+  
+}
